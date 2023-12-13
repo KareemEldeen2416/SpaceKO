c289975da7288029476507face7cf62a3cf3b30c
KareemEldeen Ahmed <kareemeldeenahmed@gmail.com>
2023-12-13 08:08:30 -0800

    Report,Missions designed

--- src/project/Project.java
+++ src/project/Project.java
@@ -26,7 +26,7 @@ public class Project extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Missions.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MissionsPersonnel.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Login");
         stage.setResizable(false);
